82b1bcddda1f226f486bdb76fb2d29e75c923f38
lenisusanti <84660880+lenisusanti@users.noreply.github.com>
2021-06-04 15:49:55 +0700

    Update index.js

--- api/index.js
+++ api/index.js
@@ -7,7 +7,7 @@ const cls_model = require('./sdk/cls_model.js');
 
 // Bot Setting
 const TelegramBot = require('node-telegram-bot-api');
-const token = '1877669223:AAFhUivKLRH-FVt7CeEuJEOfcV-sibI1xiY'
+const token = '1840956317:AAELBDSpn0W5KbKDu8r8ErV37Cg4uEerba8'
 const bot = new TelegramBot(token, {polling: true});
 
 
